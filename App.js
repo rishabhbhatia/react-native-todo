ec2a237ba74f505bd50617699c5aaf6da0415326
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2017-08-22 15:09:38 +0530

    Display todos using list component  & add new todos using input component

--- App.js
+++ App.js
@@ -7,9 +7,22 @@ import TODO_ACTIONS from './app/redux/actions/TodoActions';
 
 import Title from './app/components/Title';
 import Input from './app/components/Input';
+import List from './app/components/List';
 
 
 export default class App extends React.Component {
+
+  componentWillMount() {
+    const {store} = this.props;
+
+    const {todos} = store.getState();
+    this.setState({todos})
+
+    this.unsubscribe = store.subscribe(() => {
+      const {todos} = store.getState();
+      this.setState({todos});
+    })
+  }
 
   onAddTodo = (todo) => {
     const {store} = this.props;
@@ -19,13 +32,23 @@ export default class App extends React.Component {
     todoStore.dispatch(add_todo_action);
   }
 
+  onTodoClicked = (index) => {
+    console.log('Todo clicked:', index);
+  }
+
   render() {
+    const {todos} = this.state;
+
     return (
       <View style={styles.container}>
         <Title>My Todo List!</Title>
         <Input
           placeholder={'Type a todo, then hit enter!'}
           onSubmitEditing={this.onAddTodo}
+        />
+        <List
+          list={todos}
+          onPressItem={this.onTodoClicked}
         />
       </View>
     );
